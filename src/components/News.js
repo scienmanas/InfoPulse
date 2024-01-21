0683d00b41775545c2c2d6e219a71c780924b8ce
Manas <iamscientistmanas@gmail.com>
2024-01-21 16:08:15 +0530

    check key

--- src/components/News.js
+++ src/components/News.js
@@ -3,14 +3,56 @@ import NewsItems from './NewsItems'
 import './style/newsitem.css'
 
 export default class News extends Component {
+
+
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false
+    }
+  }
+
+  async componentDidMount() {
+    console.log("cdm");
+    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=1fcc98de2abe4fcc8e7ac9c339b4c4f6";
+
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData)
+
+    this.setState(
+      {
+        articles: parsedData.articles
+      }
+    )
+  }
+
+
   render() {
     return (
       <div className='all-news'>
         <div className="news-cards flex flex-wrap flex-row justify-evenly sm:justify-start gap-4 p-5">
-         < NewsItems />
-
+        {this.state.articles.map((element) => {
+            return   < NewsItems title={element.title} description={element.description} type="News" imgUrl={element.urlToImage} url={element.url} author="rgr" date={element.publishedAt} />
+          })}
         </div>
       </div>
     )
   }
+
+
 }
+
+
+//   render() {
+//     return (
+//       <div className='all-news'>
+//         <div className="news-cards flex flex-wrap flex-row justify-evenly sm:justify-start gap-4 p-5">
+//           {this.state.articles.map((element) => {
+//             return   < NewsItems title={element.title} description={element.description} type="News" imgUrl={element.urlToImage} url={element.url} author="rgr" date={element.publishedAt} />
+//           })}
+//         </div>
+//       </div>
+//     )
+//   }
--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -6,34 +6,37 @@ import './style/newsitem.css'
 export default class NewsItems extends Component {
 
   render() {
-    // let {title, description, imgUrl} = this.props;
+    let { title, description, type, imgUrl , url, author ,date} = this.props;
     return (
       <div className="card pb-[2px] bg-white-200 shadow-2xl rounded-lg flex flex-col gap-y-3 w-[320px] items-center h-[470px]">
         <div className="image h-auto w-auto justify-normal">
-          <img src="https://ichef.bbci.co.uk/news/1024/branded_news/184F5/production/_132337599_gettyimages-1161147027.jpg"
-            className='rounded-t-lg'
+          <img src={imgUrl}
+            className='rounded-t-lg pointer-events-none'
             alt="news"
           />
         </div>
         <div className="text-content px-3 h-full flex flex-col justify-between ">
           <div className="up flex flex-col">
-            <p className='category flex flex-row items-center gap-x-1'><TfiAnnouncement className='text-slate-700' /><span className='text-blue-600 animate-pulse text-sm font-bold type-font'>News Mania</span> </p>
-            <a href="#">
-              <h1 className='title-font mt-1'>Sachin Tendulkar: Indian cricket legend warns of 'disturbing' deepfake video
+            <p className='category flex flex-row items-center gap-x-1'><TfiAnnouncement className='text-slate-700 ' /><span className='text-blue-600 animate-pulse text-sm font-bold type-font select-none'>{type}</span> </p>
+            <a href="">
+              <h1 className='title-font mt-1'>
+                {title}
               </h1>
             </a>
-            <p className='description-font mt-6'>The fake video appears to show Sachin Tendulkar promoting an online gaming app</p>
+            <p className='description-font mt-6'>
+              {description}
+            </p>
           </div>
           <div className="bottoms flex flex-col">
             <div className="line w-full rounded-xl p-0 h-[1.5px] bg-slate-400"></div>
             <div className="content-down items-center justify-between py-1 flex flex-row">
-              <div className="date text-2xl font-date">21 Jan 2024</div>
+              <div className="date text-2xl font-date">{date}</div>
               <div className="links flex flex-row items-center gap-2">
                 <div className="author-news w-fit h-fit">
-                  <p className='w-fit h-fit text-sm font-source'>BBC News</p>
+                  <p className='w-fit h-fit text-sm font-source'>{author}</p>
                 </div>
                 <div className="source-link">
-                  <a href="#"><CiGlobe className='text-xl' /></a>
+                  <a href={url}><CiGlobe className='text-xl active:scale-75 duration-100' /></a>
                 </div>
               </div>
             </div>
