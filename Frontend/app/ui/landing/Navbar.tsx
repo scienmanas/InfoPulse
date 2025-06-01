334c877dafd367f59389477bd38fd29d1655975d
Manas <99756067+scienmanas@users.noreply.github.com>
2025-06-01 10:05:32 +0530

    Update Navbar.tsx

--- Frontend/app/ui/landing/Navbar.tsx
+++ Frontend/app/ui/landing/Navbar.tsx
@@ -6,7 +6,9 @@ import logo from "@/public/assets/logo/logo.png";
 import { FiGithub } from "react-icons/fi";
 
 export function Navbar(): JSX.Element {
-  const repoLink: string = "https://github.com/scienmanas/InfoPulse";
+  // const repoLink: string = "https://github.com/scienmanas/InfoPulse";
+    const repoLink: string = "";
+
 
   return (
     <div className="relative nav-wrapper w-fit flex items-center mt-4 lg:gap-10 border-2 rounded-2xl px-4 py-2 sm:px-7 sm:py-3 bg-[#27223b] border-yellow-500 gap-5">
